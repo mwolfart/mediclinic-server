b3851f6aecc12601cd26831367c1bf8cbc9606d6
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-04-04 15:20:35 -0300

    Fix Dockerfile; use ssl for db

--- setup/db.go
+++ setup/db.go
@@ -14,7 +14,7 @@ func StartDB() (*sql.DB, error) {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
