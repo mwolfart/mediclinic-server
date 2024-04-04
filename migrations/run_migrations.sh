89d349473be7ab83c823183c6c4d132421bef61c
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-04-04 15:23:16 -0300

    Fix ssl option in migrations

--- migrations/run_migrations.sh
+++ migrations/run_migrations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-goose postgres "host=$DB_HOSTNAME port=$DB_PORT user=$DB_USERNAME password=$DB_PASSWORD dbname=$DB_NAME sslmode=disable" up
+goose postgres "host=$DB_HOSTNAME port=$DB_PORT user=$DB_USERNAME password=$DB_PASSWORD dbname=$DB_NAME sslmode=require" up
